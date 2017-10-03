cf3b26238030a96ff359eae53839313e2dc7a9fa
Amrutha Venugopal <amruthav@qburst.com>
2017-10-03 17:05:46 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,28 @@ import { FooterComponent } from './footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule, Routes} from '@angular/router';
 import {UserService} from './user/user.service';
+import { AuthService } from "angular4-social-login";
 import {AuthguardGuard} from './authguard.guard';
 import { HttpClientModule } from '@angular/common/http';
 import { RegisterComponent } from './register/register.component';
-import {GoogleSignInComponent} from 'angular-google-signin';
+import { GoogleSigninComponent } from './google-signin/google-signin.component';
+import { SocialLoginModule } from "angular4-social-login";
+import { AuthServiceConfig, GoogleLoginProvider, FacebookLoginProvider } from 'angular4-social-login';
+
+let config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider("624796833023-clhjgupm0pu6vgga7k5i5bsfp6qp6egh.apps.googleusercontent.com")
+  },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider("561602290896109")
+  }
+]);
+
+export function provideConfig() {
+  return config;
+}
 
 const appRoutes:Routes = [
 {
@@ -26,8 +44,14 @@ const appRoutes:Routes = [
   path:'dashboard',
   canActivate: [AuthguardGuard],
   component: DashboardComponent
+},
+{
+  path:'sign-in',
+  component: GoogleSigninComponent
 }
 ]
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,13 +59,16 @@ const appRoutes:Routes = [
     LoginFormComponent,
     FooterComponent,
     DashboardComponent,
-    RegisterComponent,
-    GoogleSignInComponent
+    RegisterComponent,GoogleSigninComponent
+    
   ],
   imports: [
-    BrowserModule,RouterModule.forRoot(appRoutes),HttpClientModule        
+    BrowserModule,RouterModule.forRoot(appRoutes),HttpClientModule,SocialLoginModule       
   ],
-  providers: [UserService,AuthguardGuard],
+  providers: [UserService,AuthguardGuard,AuthService,{
+      provide: AuthServiceConfig,
+      useFactory: provideConfig
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
